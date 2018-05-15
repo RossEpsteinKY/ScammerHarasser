f9b7830251080439210f7a4569f9949622c0ebea
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2018-05-15 12:42:08 -0400

    Update scammer_harasser.py

--- scammer_harasser.py
+++ scammer_harasser.py
@@ -2,25 +2,28 @@ from twilio.rest import TwilioRestClient
 import time
 
 #Your twilio number
-TWILIO_PHONE_NUMBER = ""
+YOUR_NUMBER = ""
 
 #Put mass call number(s) here
-DIAL_NUMBERS = ["",]
+CHOSEN_NUMBERS = ["",]
 
 # URL location of TwiML instructions for how to handle the phone call
-TWIML_INSTRUCTIONS_URL = \
+XML_URL = \
   ""
 
 #your twilio info
 client = TwilioRestClient("", "")
 
 
-def dial_numbers(numbers_list):
-    for number in numbers_list:
+def call_number(numbers):
+    for number in numbers:
         print("Calling the following phone number:  " + number)
 
-        client.calls.create(to=number, from_=TWILIO_PHONE_NUMBER,
-                            url=TWIML_INSTRUCTIONS_URL, method="GET")
+        client.calls.create(to=number, from_=YOUR_NUMBER,
+                            url=XML_URL, method="GET")
+
+
+
     
 def program_start():
     keepcalling = input("Would you like to place indefinite calls?   >")
@@ -29,17 +32,20 @@ def program_start():
     else:
         print("Say 'yes' when ready")
         program_start()
-     
+        
+        
+
 def program_loop():
     loop_dial()        
     time.sleep(10)
     run_again()
 
 def loop_dial():
-    dial_numbers(DIAL_NUMBERS)
+    call_number(CHOSEN_NUMBERS)
 
 def run_again():
     program_loop()
+            
 
 program_start()
 
